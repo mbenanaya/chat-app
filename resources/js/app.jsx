1872ecc172c2e75dd911d05f3a51c972eaa8c4e4
mbenanaya <mouhcinebenanaya19@gmail.com>
2023-08-31 20:38:23 +0100

    Initial commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,43 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#19d71f',
+    },
+});
+
+Echo.private('messenger')
+    .listen('MessageSent', (e) => {
+        console.log(e.message);
+    });
+
+Echo.join(`group_chat.1`)
+    .here((users) => {
+        console.log(users)
+    })
+    .joining((user) => {
+        console.log(user.name);
+    })
+    .leaving((user) => {
+        console.log(user.name);
+    })
+    .listen('GroupChatMessage', (e) => {
+        console.log(e);
+    })
+    .error((error) => {
+        console.error(error);
+    });
